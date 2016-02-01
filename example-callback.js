ef21f67dc30b95829109728a29ec619de857cc54
Jorge Gallardo <jorge8486@gmail.com>
2016-02-01 00:23:23 -0500

    asynchronous programming

---
+++ example-callback.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=Bethpage,NY&units=imperial&appid=44db6a862fba0b067b1930da0d769e98';
+
+request({
+  url: url,
+  json: true
+}, function(error, response, body) {
+  if(error) {
+    console.log('Unable to fetch weather.');
+  } else {
+    // console.log(JSON.stringify(body, null, 4));
+    // It is X degrees in Astoria
+    console.log('It is ' + body.main.temp + ' degrees in ' + body.name + '!');
+  }
+});
